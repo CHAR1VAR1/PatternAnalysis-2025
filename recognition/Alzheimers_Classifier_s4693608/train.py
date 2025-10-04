fd25c4eae6940367a94507ea645e0b6382004da4
CHAR1VAR1 <cvever11@gmail.com>
2025-10-04 13:32:25 +1000

    Implemented training loop

--- recognition/Alzheimers_Classifier_s4693608/train.py
+++ recognition/Alzheimers_Classifier_s4693608/train.py
@@ -6,7 +6,7 @@ from dataset import get_dataloaders
 from modules import AlzheimersClassifier
 import matplotlib.pyplot as plt
 
-def train_model(root_dir, epochs=10, batch_szie=16, lr=1e-4, device='cuda'):
+def train_model(root_dir, epochs=10, batch_size=16, lr=1e-4, device='cuda'):
     """
     Train and evaluate a deep learning model for Alzheimer's disease classification.
 
@@ -30,3 +30,30 @@ def train_model(root_dir, epochs=10, batch_szie=16, lr=1e-4, device='cuda'):
             - "val_acc_curve.png" for validation accuracy
         - Prints training/validation progress and best validation accuracy to the console.
     """
+    train_loader, test_loader = get_dataloaders(root_dir, batch_size=batch_size)
+
+    # Define model, loss, optimiser
+    model = AlzheimersClassifier().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimiser = optim.AdamW(model.parameters(), lr=lr)
+
+    train_losses, val_losses = val_accs = [], [], []
+    best_acc = 0.0
+
+    for epoch in range(epochs):
+        # Training
+        model.train()
+        running_loss = 0.0
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            optimiser.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimiser.step()
+
+            running_loss += loss.item()
+
+        train_loss = running_loss / len(train_loader)
+        train_losses.append(train_loss)
