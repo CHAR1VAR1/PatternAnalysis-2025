c01b060f02c91d1231f608132b108d7564d01e21
CHAR1VAR1 <cvever11@gmail.com>
2025-10-07 15:47:20 +1000

    Implemented evaluation of whole folders of AD/NC images

--- recognition/Alzheimers_Classifier_s4693608/predict.py
+++ recognition/Alzheimers_Classifier_s4693608/predict.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torchvision import transforms
 from PIL import Image
@@ -36,7 +37,7 @@ def predict_image(image_path, model):
 
     Returns:
         tuple:
-            - label (str): Predicted class label ("AD" or "NC").
+            - prediction (int): Predicted class label value (1 for "AD" or 0 for "NC").
             - confidence (float): Model confidence for the predicted class,
                                   between 0.0 and 1.0.
     """
@@ -58,9 +59,52 @@ def predict_image(image_path, model):
     class_names = ["NC", "AD"]
     label = class_names[pred.item()]
     print(f"Prediction: {label} ({conf.item() * 100:.2f}% confidence)")
-    return label, conf.item()
+    return pred.item(), conf.item()
+
+def evaluate_folder(model, folder_path, label):
+    """
+    This function iterates through all `.jpeg` files in the specified folder,
+    performs inference using the provided model, and counts how many predictions
+    match the expected label.
+
+    Args:
+        model : The trained PyTorch model used for prediction.
+        folder_path (str): Path to the folder containing `.jpeg` images to evaluate.
+        label (int): The ground truth label for all images in this folder
+
+    Returns:
+        correct (int): Number of correctly classified images.
+        total (int): Total number of images evaluated.
+        conf_sum (float): Current sum of confidence of classifcation of images in the folder.
+    """
+    # label = 1 for AD, label = 0 for NC
+    correct = total = conf_sum = 0
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        pred, conf = predict_image(fpath, model)
+        total += 1
+        correct += int(pred == label)
+        conf_sum += conf.item()
+    return correct, total, conf_sum
 
 if __name__ == "__main__":
     model = load_model()
-    test_image_path = "/home/groups/comp3710/ADNI/AD_NC/test/AD/388206_78.jpeg"
-    predict_image(test_image_path, model)
+
+    ad_path = "/home/groups/comp3710/ADNI/AD_NC/test/AD"
+    nc_path = ad_path = "/home/groups/comp3710/ADNI/AD_NC/test/NC"
+    
+    ad_correct, ad_total, ad_conf_sum = evaluate_folder(model, ad_path, label = 1)
+    nc_correct, nc_total, nc_conf_sum = evaluate_folder(model, nc_path, label = 0)
+
+    total_correct = ad_correct + nc_correct
+    total_images = ad_total + nc_total
+    accuracy = total_correct / total_images
+
+    print(f"\n ----- Evaluation Results -----")
+    print(f"AD: {ad_correct}/{ad_total} correct ({ad_correct / ad_total * 100:.2f}%)")
+    print(f"NC: {nc_correct}/{nc_total} correct ({nc_correct / nc_total * 100:.2f}%)")
+    print(f"Overall Accuracy: {accuracy*100:.2f}%")
+    print(f"\n ----- Average Confidence Results -----")
+    print(f"AD: {ad_conf_sum / ad_total * 100:.2f}%")
+    print(f"NC: {nc_conf_sum / nc_total * 100:.2f}%")
+    print(f"Total: {(ad_conf_sum + nc_conf_sum) / total_images * 100:.2f}")
